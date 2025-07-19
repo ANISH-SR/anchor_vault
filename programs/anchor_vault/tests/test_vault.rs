90f56a9ec0003a80ba831faace979c7ecd6c94cd
ANISH-SR <rawatanish08@gmail.com>
2025-07-19 19:35:46 +0000

    [CHORE] Removes redudant testing code

--- programs/anchor_vault/tests/test_vault.rs
+++ programs/anchor_vault/tests/test_vault.rs
@@ -23,56 +23,6 @@ mod tests {
 
     const ID: pubkey::Pubkey = pubkey!("FGxVc2HAfo2bDARNMtDRzKwCbRCT8XpvBiYUYqPjLhqt");
     const USER: pubkey::Pubkey = pubkey::Pubkey::new_from_array([0x01; 32]);
-
-    #[test]
-    fn test_initialize() {
-        let mollusk = Mollusk::new(&ID, "../../target/deploy/anchor_vault");
-
-        let (state_pda, state_bump) =
-            Pubkey::find_program_address(&[b"state", USER.as_ref()], &ID);
-        let (vault_pda, vault_bump) =
-            Pubkey::find_program_address(&[b"vault", state_pda.as_ref()], &ID);
-
-        let (system_program, system_account) = program::keyed_account_for_system_program();
-
-        // User: some lamports, owned by system
-        let user_account = Account::new(1 * LAMPORTS_PER_SOL, 0, &system_program);
-
-        // State account: rent exempt, space = 2 bytes, owned by program
-        let state_account = Account::new(
-            Rent::default().minimum_balance(8 + 2), // discriminator + VaultState (2 bytes)
-            8 + 2,
-            &ID,
-        );
-
-        // Vault: just needs to exist and receive lamports, also owned by program
-        let vault_account = Account::new(
-            Rent::default().minimum_balance(0),
-            0,
-            &ID,
-        );
-
-        let ix_accs = vec![
-            AccountMeta::new(USER, true),
-            AccountMeta::new(state_pda, false),
-            AccountMeta::new(vault_pda, false),
-            AccountMeta::new_readonly(system_program, false),
-        ];
-
-        let data = instruction::Initialize {}.data();
-
-        let ix = Instruction::new_with_bytes(ID, &data, ix_accs);
-
-        let tx_accounts = vec![
-            (USER, user_account),
-            (state_pda, state_account),
-            (vault_pda, vault_account),
-            (system_program, system_account),
-        ];
-
-        mollusk.process_and_validate_instruction(&ix, &tx_accounts, &[Check::success()]);
-    }
-
     
     #[test]
     fn test_deposit() {
@@ -89,40 +39,65 @@ mod tests {
         let (system_program, system_account) = program::keyed_account_for_system_program();
 
         //Build the accounts
-        let user_accounts = Account::new(1 * LAMPORTS_PER_SOL, 0, &system_program);
+        let user_account = Account::new(2 * LAMPORTS_PER_SOL, 0, &system_program);
         let state_account = Account::new(0, 0, &system_program);
         let vault_account = Account::new(0, 0, &system_program);
 
         //get the accounts meta
-        let ix_accs = vec![
+        let init_ix_accs = vec![
             AccountMeta::new(USER, true),
             AccountMeta::new(state_pda, false),
             AccountMeta::new(vault_pda, false),
             AccountMeta::new_readonly(system_program, false),
         ];
 
-        //Data
-        let transfer_amount: u64 = 500_000_000;
-        let data = (anchor_vault::instruction::Deposit {
-            amount: transfer_amount,
-        })
-        .data();
+        let init_data = instruction::Initialize {}.data();
+        let init_ix = Instruction::new_with_bytes(ID, &init_data, init_ix_accs);
 
-        //Build Ix
-        let instructions = Instruction::new_with_bytes(ID, &data, ix_accs);
-
-        //Get Tx Accounts
-        let tx_accs = &vec![
-            (USER, user_accounts.clone()),
-            (state_pda, state_account.clone()),    //system_account.clone()
-            (vault_pda, vault_account.clone()),
+        let init_tx_accounts = vec![
+            (USER, user_account.clone()),
+            (state_pda, state_account),
+            (vault_pda, vault_account),
             (system_program, system_account.clone()),
         ];
 
+        // Execute initialize
+        let result = mollusk.process_instruction(&init_ix, &init_tx_accounts);
+
+        // Step 2: Extract accounts after initialize for deposit test
+        // Extract updated accounts from the result
+        let post_user_account = result.get_account(&USER).unwrap().clone();
+        let post_state_account = result.get_account(&state_pda).unwrap().clone();
+        let post_vault_account = result.get_account(&vault_pda).unwrap().clone();
+
+        //Data
+        let deposit_amount: u64 = 500_000_000;
+        let data = (anchor_vault::instruction::Deposit {
+            amount: deposit_amount,
+        })
+        .data();
+
+        let deposit_ix_accs = vec![
+            AccountMeta::new(USER, true),
+            AccountMeta::new(state_pda, false),
+            AccountMeta::new(vault_pda, false),
+            AccountMeta::new_readonly(system_program, false),
+        ];
+
+        //Build Ix
+        let instructions = Instruction::new_with_bytes(ID, &data, deposit_ix_accs);
+
+        //Get Tx Accounts
+        let tx_accs = &vec![
+            (USER, post_user_account),
+            (state_pda, post_state_account),    //system_account.clone()
+            (vault_pda, post_vault_account),
+            (system_program, system_account),
+        ];
+
         //process and validate our instruction
-        let test_deposit = 
-            mollusk.process_and_validate_instruction(&instructions, &tx_accs, &[Check::success()]);
-        
+        // let test_deposit = 
+        mollusk.process_and_validate_instruction(&instructions, &tx_accs, &[Check::success()]);
     }
 
     #[test]
